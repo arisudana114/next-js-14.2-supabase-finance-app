743efcba766e2b73224052ebdb24ea56a5721e70
Ari Sudana <arisudana114@gmail.com>
2024-05-29 21:12:00 +0700

    added loading to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div>
-        <h1 className="text-4xl">Welcome to Finance App!</h1>
+      <div className="p-4">
+        <h1 className="text-4xl font-bold flex flex-col gap-4">Welcome to Finance App!</h1>
         <Button>
           <Link href="/dashboard">Go to Finance Dashboard</Link>
         </Button>
